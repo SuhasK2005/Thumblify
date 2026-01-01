73a77237be3e0eeea72e9705c95abda762dcd9fd
SuhasK2005 <saisuhask2005@gmail.com>
2026-01-01 21:39:06 +0530

    Implemented search parameter handling in YtPreview component

--- client/src/pages/YtPreview.tsx
+++ client/src/pages/YtPreview.tsx
@@ -1,7 +1,12 @@
+import { useSearchParams } from "react-router-dom";
+import { yt_html } from "../assets/assets";
+
 const YtPreview = () => {
-  return <div>
-    
-  </div>;
+  const [searchParams] = useSearchParams()
+  
+  return (
+  <div></div>
+);
 };
 
 export default YtPreview;
