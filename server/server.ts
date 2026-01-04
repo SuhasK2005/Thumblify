68e5fe157de022396ffc32ab85af765c82a09cae
SuhasK2005 <saisuhask2005@gmail.com>
2026-01-04 16:16:27 +0530

    Updated CORS origin to use the correct Vercel deployment URL

--- server/server.ts
+++ server/server.ts
@@ -23,7 +23,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://thumblify-olive.vercel.app",
+      "https://thumblify-sk.vercel.app",
     ],
     credentials: true,
   })
